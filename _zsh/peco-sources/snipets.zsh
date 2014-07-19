d27a7fe22ad791d547fb1a87bc1b8d70f15e4184
linyows <linyows@gmail.com>
2014-07-19 22:45:13 +0900

    add snipets.zsh

---
+++ _zsh/peco-sources/snipets.zsh
@@ -0,0 +1,11 @@
+function peco-snippets() {
+    if [ -e ~/.config/snipets ]; then
+        local selected=$(grep -v "^#" ~/.config/snipets | peco --query "$LBUFFER")
+        if [ -n "$selected" ]; then
+            BUFFER="${selected}"
+            CURSOR=$#BUFFER
+        fi
+        zle clear-screen
+    fi
+}
+zle -N peco-snippets

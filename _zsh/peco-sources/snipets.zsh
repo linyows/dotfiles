2d6a0807ce9a430a40917526a60b929d2de9cfdb
linyows <linyows@gmail.com>
2014-07-21 11:52:05 +0900

    to straight path

--- _zsh/peco-sources/snipets.zsh
+++ _zsh/peco-sources/snipets.zsh
@@ -1,6 +1,6 @@
 function peco-snippets() {
-    if [ -e ~/.config/snipets ]; then
-        local selected=$(grep -v "^#" ~/.config/snipets | peco --query "$LBUFFER")
+    if [ -e ~/.dotfiles/_config/snipets ]; then
+        local selected=$(grep -v "^#" ~/.dotfiles/_config/snipets | peco --query "$LBUFFER")
         if [ -n "$selected" ]; then
             BUFFER="${selected}"
             CURSOR=$#BUFFER

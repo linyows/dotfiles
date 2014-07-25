48bfa3731ed5603b9a60f07a1094394547b6009b
linyows <linyows@gmail.com>
2014-07-25 19:36:58 +0900

    :bug:

--- _zsh/peco-sources/snipets.zsh
+++ _zsh/peco-sources/snipets.zsh
@@ -1,7 +1,10 @@
 function peco-snippets() {
     if [ -e ~/.dotfiles/_config/snipets ]; then
-        local selected=$(grep -v "^#" ~/.dotfiles/_config/snipets | peco --query "$LBUFFER")
+        local selected="$(grep -v "^#" ~/.dotfiles/_config/snipets | peco --query "$LBUFFER")"
         if [ -n "$selected" ]; then
+            if [ -x "`which pbcopy`" ]; then
+                echo "${selected}" | pbcopy
+            fi
             BUFFER="${selected}"
             CURSOR=$#BUFFER
         fi

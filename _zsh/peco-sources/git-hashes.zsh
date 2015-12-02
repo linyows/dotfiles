ef9a83ae24fe1b972e9c4a92e4b28a2fc4d390ce
linyows <linyows@gmail.com>
2015-12-02 10:32:29 +0900

    use clipboard if there is pbcopy

--- _zsh/peco-sources/git-hashes.zsh
+++ _zsh/peco-sources/git-hashes.zsh
@@ -1,9 +1,14 @@
 function peco-git-hashes () {
-    local selected="$(git log --format="%ai  %an  %h  %d %s" | peco | perl -pe 's/\n//g')"
+    local selected="$(git log --format="%ai  %an  %h  %d %s" | peco)"
     if [ -n "$selected" ]; then
         local remote="$(git remote -v | grep fetch | awk '{print $2}' | perl -pe 's/:/\//' | perl -pe 's/git@/https:\/\//' | perl -pe 's/\.git$//')"
-        BUFFER="open ${remote}/commit/$(echo $selected | awk '{print $5}')"
-        CURSOR=$#BUFFER
+        local url="${remote}/commit/$(echo $selected | awk '{print $5}')"
+        if [ -x "`which pbcopy`" ]; then
+            echo "${url}" | pbcopy
+        else
+            BUFFER="open ${url})"
+            CURSOR=$#BUFFER
+        fi
     fi
     zle clear-screen
 }

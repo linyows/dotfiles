1c08eba5b33ac37f1472dacd77c21ff8406ea3e5
linyows <linyows@gmail.com>
2015-12-02 10:50:48 +0900

    open and set url to clipbord

--- _zsh/peco-sources/git-hashes.zsh
+++ _zsh/peco-sources/git-hashes.zsh
@@ -1,12 +1,18 @@
 function peco-git-hashes () {
     local selected="$(git log --format="%ai  %an  %h  %d %s" | peco)"
     if [ -n "$selected" ]; then
-        local remote="$(git remote -v | grep fetch | awk '{print $2}' | perl -pe 's/:/\//' | perl -pe 's/git@/https:\/\//' | perl -pe 's/\.git$//')"
-        local url="${remote}/commit/$(echo $selected | awk '{print $5}')"
+        local remote="$(git remote -v | grep fetch | awk '{print $2}' | \
+            perl -pe 's/ssh:\/\///' | perl -pe 's/:/\//' | perl -pe 's/git@/https:\/\//' | perl -pe 's/\.git$//')"
+        local url="${remote}/commit/$(echo $selected | \
+            awk 'match($0,/[[:blank:]]{2}[a-z0-9]{7}[[:blank:]]{2}/) { print substr($0, RSTART+2, RLENGTH-2) }')"
+
         if [ -x "`which pbcopy`" ]; then
             echo "${url}" | pbcopy
+            BUFFER="open ${url}"
+            CURSOR=$#BUFFER
+            zle accept-line
         else
-            BUFFER="open ${url})"
+            BUFFER="open ${url}"
             CURSOR=$#BUFFER
         fi
     fi
